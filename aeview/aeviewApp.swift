e28fb4b42acc7376ac1484f66db345c0eb0ec517
Kevin Zheng <jxzheng@uwaterloo.ca>
2023-07-14 19:13:28 -0400

    Initial Commit

---
+++ aeview/aeviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  aeviewApp.swift
+//  aeview
+//
+//  Created by Kevin Zheng on 2023-07-14.
+//
+
+import SwiftUI
+
+@main
+struct aeviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
